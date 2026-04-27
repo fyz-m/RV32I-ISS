cc0e7caa291741b210fafd04e3a74f5f3c20c9ce
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-04-27 11:24:19 +0400

    Add Class for Memory

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,5 +1,6 @@
 #include <cstdint>
 #include <array>
+#include <vector>
 
 // Register File Class
 
@@ -10,11 +11,18 @@
 class RegisterFile
 {
   private:
-     std::array<uint32_t, 32> m_registers; // RISC-V architecture contains 32 registers
+
+     std::array <uint32_t, 32> m_registers{}; // RISC-V architecture contains 32 registers
+     
   public:
+
+    RegisterFile()
+    {
+      
+    }
     
     // TODO: add error checking for out of bounds index
-    void regWrite(uint32_t data, int address)
+    void Write(uint32_t data, int address)
     {
       // Register x0 is hardwired to 0 so writes to the first register are ignored
       if (address == 0)
@@ -23,7 +31,7 @@ class RegisterFile
       m_registers[address] = data;
     }
 
-    uint32_t regRead(int address)
+    uint32_t Read(int address)
     {
       if (address == 0)
         return 0;
@@ -37,4 +45,58 @@ class RegisterFile
 
 
 // Memory Class (32-bit so max 4GB)
-// Method to write and read
+class Memory
+{
+
+  private:
+    // byte-addressable memeory 
+    std::vector <uint8_t> m_memory;
+    int WORD_SIZE = 32;
+  public:
+    
+    Memory()
+    {
+      m_memory.reserve(512);
+    }
+    Memory(int size)
+    {
+      m_memory.reserve(size);
+    }
+
+    void Write(uint8_t data, int address)
+    {
+      // Write a byte
+      m_memory[address] = data;
+    }
+    
+    void Write(uint32_t data, int address)
+    {
+      // Write a word (32-bit)
+  
+      for (int i = 0; i < 4; i++)
+      {
+        uint8_t data_byte = data >> (i * 8); 
+        Write(data_byte, address + i); 
+      }
+
+    }
+
+    uint8_t Read_Byte(int address)
+    {
+      return m_memory[address];
+    }
+
+    uint32_t Read_Word(int address)
+    {
+      uint32_t word {};
+
+      for (int i = 0; i < 4; i++)
+      {
+        auto byte = static_cast<uint32_t>(Read_Byte(address + i));
+
+        word |= byte << (8 * i);
+      }
+      return word;
+    }
+};
+
