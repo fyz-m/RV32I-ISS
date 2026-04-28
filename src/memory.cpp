2ec15d37210512bf9c2d5def0fdd438013a742af
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-04-28 07:35:13 +0400

    Add method to load binary file into memory object

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,26 +1,22 @@
 #include <cstdint>
 #include <array>
+#include <fstream>
+#include <iostream>
+#include <ostream>
 #include <vector>
+#include <stdio.h>
 
-// Register File Class
 
-// ignore writes to regfile[0]
-// In RISC-V register x0 is hardwired to 0
+constexpr int REGISTER_FILE_SIZE = 32; 
 
-// Method to write and read the register file
 class RegisterFile
 {
   private:
 
-     std::array <uint32_t, 32> m_registers{}; // RISC-V architecture contains 32 registers
+     std::array <uint32_t, REGISTER_FILE_SIZE> m_registers{}; // RISC-V architecture contains 32 registers
      
   public:
 
-    RegisterFile()
-    {
-      
-    }
-    
     // TODO: add error checking for out of bounds index
     void Write(uint32_t data, int address)
     {
@@ -32,10 +28,7 @@ class RegisterFile
     }
 
     uint32_t Read(int address)
-    {
-      if (address == 0)
-        return 0;
-
+    {      
       return m_registers[address];
     }
     
@@ -45,22 +38,26 @@ class RegisterFile
 
 
 // Memory Class (32-bit so max 4GB)
+// TODO:
+// out of bounds checking
+// read/write half words (16 bits)
 class Memory
 {
 
   private:
-    // byte-addressable memeory 
-    std::vector <uint8_t> m_memory;
-    int WORD_SIZE = 32;
+    // byte-addressable memeory so memory must be an array of bytes
+    std::vector <uint8_t> m_memory {};
+    int m_WORD_SIZE;
+    int  m_INSTRUCTION_SIZE;
+
   public:
     
-    Memory()
-    {
-      m_memory.reserve(512);
-    }
-    Memory(int size)
+    Memory(int size = 512, int word_size = 32)
+      : m_WORD_SIZE {word_size},
+        m_INSTRUCTION_SIZE {word_size / 8} // number of bytes 
     {
       m_memory.reserve(size);
+      
     }
 
     void Write(uint8_t data, int address)
@@ -98,5 +95,25 @@ class Memory
       }
       return word;
     }
-};
 
+     bool Load(const char *file)
+    {
+      // Load contents of a file into the memory
+      std::ifstream input_file{file, std::ios::binary};
+
+      if (!input_file)
+      {
+          std::cerr << "Could not open file";
+          return false;
+      }
+      
+      uint32_t inst;
+      while (input_file.read( reinterpret_cast<char*>(&inst), sizeof(inst)))
+      {
+        m_memory.push_back(inst);
+      }
+
+      input_file.close();
+      return true;
+    }
+};
