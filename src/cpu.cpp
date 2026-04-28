f79aa027a997904ec7e1991ffc1e178051888598
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-04-28 09:35:01 +0400

    Add CPU class

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -1,50 +1,38 @@
 #pragma  once
 #include "headers/memory.hpp"
+#include "headers/cpu.hpp"
 
-
-
-class CPU
-{
-  private:
-
-    uint32_t m_programCounter;
-    RegisterFile m_registerFile;
-
-  public:
-
-    CPU(int width = 32)
+    CPU::CPU(int)
     : m_programCounter {},
-      m_registerFile()
-      
+      m_registerFile()      
     {
 
     }
 
-    void PC_write(uint32_t data)
+    void CPU::PC_write(uint32_t data)
     {
       // Write to the PC for branch and jump instructions
       m_programCounter = data;
     }
 
-    void PC_increment(void)
+    void CPU::PC_increment(void)
     {
       m_programCounter += 4;
     }
 
-    uint32_t PC_read(void)
+    uint32_t CPU::PC_read(void)
     {
       return m_programCounter;
     }
 
-    void Reg_write(uint32_t data, int address)
+    void CPU::Reg_write(uint32_t data, int address)
     {
       m_registerFile.Write(data, address);
     }
 
-    uint32_t Reg_read(int address)
+    uint32_t CPU::Reg_read(int address)
     {
      return m_registerFile.Read(address);
     }
 
-};
 
