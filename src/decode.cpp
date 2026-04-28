89e52bba0e1f9c4c66317f3d452db816a3268de5
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-04-28 19:57:38 +0400

    Add R-type instruction decoding

--- src/decode.cpp
+++ src/decode.cpp
@@ -1,4 +1,5 @@
 #include "headers/decode.hpp"
+#include "headers/memory.hpp"
 #include <iostream>
 
 
@@ -38,30 +39,43 @@ void decode(Instruction* instruction) {
 
 void decode_R_type(Instruction* instruction)
 {
+  // extract fields
+  instruction->rd = (instruction->instruction >> 7) & 0x000001F;
+  instruction->funct3 = (instruction->instruction >> 12) & 0x00007; // 0b0000'0000'0000'0000'0111
+  instruction->rs1 = (instruction->instruction >> 15) & 0x0001F; // 0b0000'0000'0000'1111'1
+  instruction->rs2 = (instruction->instruction >> 20) & 0x01F; // 0b0000'0001'1111
+  instruction->funct7 = (instruction->instruction >> 25);
+
+  // Set operation
+  switch (instruction->funct3) {
+    case 0b000:
+        instruction->Operation = (instruction->funct7 == 0) ?  OPERATION::ADD : OPERATION::SUB;
   
+  }
+
 }
 
 void decode_I_type(Instruction* instruction)
 {
-  
+  instruction->rd = (instruction->instruction >> 7) & 0x000F;
 }
 
 void decode_S_type(Instruction* instruction)
 {
-  
+  instruction->rd = (instruction->instruction >> 7) & 0x000F;
 }
 
 void decode_B_type(Instruction* instruction)
 {
-  
+  instruction->rd = (instruction->instruction >> 7) & 0x000F;
 }
 
 void decode_U_type(Instruction* instruction)
 {
-  
+  instruction->rd = (instruction->instruction >> 7) & 0x000F;
 }
 
 void decode_J_type(Instruction* instruction)
 {
-  
+  instruction->rd = (instruction->instruction >> 7) & 0x000F;
 }
