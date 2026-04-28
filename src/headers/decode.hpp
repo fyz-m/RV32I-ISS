0f3eb3af0d11e8af4ee084cda077089ff6548516
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-04-28 16:10:54 +0400

    Add decode function prototypes

--- src/headers/decode.hpp
+++ src/headers/decode.hpp
@@ -2,7 +2,16 @@
 #include "memory.hpp"
 #include <cstdint>
 
-
-
-
 void decode(Instruction* instruction);
+
+void decode_R_type(Instruction* instruction);
+
+void decode_I_type(Instruction* instruction);
+
+void decode_S_type(Instruction* instruction);
+
+void decode_B_type(Instruction* instruction);
+
+void decode_U_type(Instruction* instruction);
+
+void decode_J_type(Instruction* instruction);
