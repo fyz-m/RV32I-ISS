ed920bf15e6c761b1580c6c820265567274778fa
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-04-28 15:35:34 +0400

    Add step and fetch functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,63 @@
-#pragma once
-#include "headers/Globals.hpp"
 #include "headers/cpu.hpp"
-#include "headers/memory.hpp"
 #include "headers/decode.hpp"
 #include "headers/execute.hpp"
+#include "headers/memory.hpp"
+#include <cstdint>
 #include <iostream>
-#include <memory>
 
 void load_file(char *filename);
-void Step();
-void fetch();
-void check_ptr();
+void Step(CPU& CPU, Memory& instruction_mem, Memory *data_mem); 
+void fetch(CPU& CPU, const Memory& instruction_mem, Instruction *inst);
 
-int main(int argc, char *argv[])
+constexpr int SIZE = 1024;
+
+
+
+int main(int argc, char *argv[]) 
 {
-  check_ptr();
-  load_file(argv[2]);
-  Step();
+
+  if (argc != 2) 
+  {
+    std::cout << "Invalid args" << std::endl;
+    return 1;
+  }
+
+  // Create CPU object with register file + pc
+  CPU CPU; 
+
+  // Create memory and load instructions
+  Memory Instruction_Memory;
+  Instruction_Memory.Load(argv[2]);
+
+  // Create memory for Data
+  Memory *Data_Memory = new Memory(SIZE);
+
+  Step(CPU, Instruction_Memory , Data_Memory);
+
 }
 
-void Step()
+void Step(CPU& CPU, const Memory& Instruction_Memory, Memory *Data_Memory) 
 {
+
+  Instruction *instr {};
+
+  int num_instr = Instruction_Memory.number_of_words();  
+
   // Loop until all instruction completed
-  fetch();
-  decode();
-  execute();
+  for (int cycles = 0; cycles < num_instr; cycles++) 
+  {
+    fetch(CPU, Instruction_Memory, instr);
+    decode(instr);
+    execute(CPU, Data_Memory, instr);
+  }
 }
 
-void fetch()
+void fetch(CPU& CPU, const Memory& instruction_mem, Instruction *inst)
 {
-  // Fetch instruction from the instruction memory 
-  // Address of instruction to fetch is the current value of the program counter
-   *instruction_register = instruction_memory->Read_Word( cpu->PC_read());
+ // Fetch the instruction and store it in the instruction register
+ // Address of instruction to fetch = value in program counter
+  inst->instruction = instruction_mem.Read_Word( CPU.PC_read()); 
 
-  // Increment program counter to next address
-  cpu->PC_increment();
-}
-
-void check_ptr()
-{
-  if (instruction_memory == nullptr ||
-      data_memory == nullptr ||
-      cpu == nullptr ||
-      instruction_register == nullptr)
-      {
-        std::cerr << "Could not initialize global pointer " << std::endl;
-      }
-}
-
-void load_file(char *filename)
-{
-  instruction_memory->Load(filename);
+  // PC += 4
+  CPU.PC_increment();
 }
