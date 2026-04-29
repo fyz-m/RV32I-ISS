e46a1b47887ef4a1f501a8cbcae444d37305e374
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-04-29 09:11:44 +0400

    Update function prototypes to use reference instead of pointer for Instruction struct

--- src/headers/decode.hpp
+++ src/headers/decode.hpp
@@ -1,17 +1,16 @@
 #pragma once
 #include "memory.hpp"
-#include <cstdint>
 
-void decode(Instruction* instruction);
+void decode(Instruction &instruction);
 
-void decode_R_type(Instruction* instruction);
+void decode_R_type(Instruction &fields);
 
-void decode_I_type(Instruction* instruction);
+void decode_I_type(Instruction &fields);
 
-void decode_S_type(Instruction* instruction);
+void decode_S_type(Instruction &fields);
 
-void decode_B_type(Instruction* instruction);
+void decode_B_type(Instruction &fields);
 
-void decode_U_type(Instruction* instruction);
+void decode_U_type(Instruction &fields);
 
-void decode_J_type(Instruction* instruction);
+void decode_J_type(Instruction &fields);
--- src/headers/execute.hpp
+++ src/headers/execute.hpp
@@ -2,4 +2,4 @@
 #include "cpu.hpp"
 #include "memory.hpp"
 
-void execute(CPU& CPU, Memory *Data_Memory, Instruction *instr);
+void execute(CPU &CPU, Memory *Data_Memory, Instruction &i);
