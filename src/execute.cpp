50097b4bbc972d49f1a2f545fadfc471226841fb
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-04-29 20:09:46 +0400

    Add execute logic for ADDI

--- src/execute.cpp
+++ src/execute.cpp
@@ -11,17 +11,20 @@ void execute(CPU &CPU, Memory *Data_Memory, Instruction &i) {
   case OPERATION::ADD:
   case OPERATION::SUB: {
 
-    auto operand1 = static_cast<int32_t>(CPU.Reg_read(i.rs1));
-    auto operand2 = static_cast<int32_t>(CPU.Reg_read(i.rs2));
+    auto operand1 = (CPU.Reg_read(i.rs1));
+    auto operand2 = (CPU.Reg_read(i.rs2));
 
     auto sum = (i.Operation == OPERATION::ADD) ? (operand1 + operand2)
                                                : (operand1 - operand2);
     CPU.Reg_write(sum, i.rd);
     break;
   }
-
-  case OPERATION::UNKNOWN:
-
+  case OPERATION::ADDI: {
+    auto sum = (CPU.Reg_read(i.rs1)) + i.imm;
+    CPU.Reg_write(sum, i.rd);
     break;
+  }
+    default:
+      break;
   }
 }
