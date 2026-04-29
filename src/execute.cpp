70b620c6be830972f5f0f55aec8f4e70bd46e5f7
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-04-29 10:05:48 +0400

    Add execution logic for SUB instruction

--- src/execute.cpp
+++ src/execute.cpp
@@ -5,19 +5,20 @@
 
 void execute(CPU &CPU, Memory *Data_Memory, Instruction &i) {
   Data_Memory->Write(static_cast<uint8_t>(32), 20);
+
   switch (i.Operation) {
 
-  case OPERATION::ADD: {
+  case OPERATION::ADD:
+  case OPERATION::SUB: {
+
     auto operand1 = static_cast<int32_t>(CPU.Reg_read(i.rs1));
     auto operand2 = static_cast<int32_t>(CPU.Reg_read(i.rs2));
-    auto sum = operand1 + operand2;
+
+    auto sum = (i.Operation == OPERATION::ADD) ? (operand1 + operand2)
+                                               : (operand1 - operand2);
     CPU.Reg_write(sum, i.rd);
     break;
   }
-
-  case OPERATION::SUB:
-
-    break;
 
   case OPERATION::UNKNOWN:
 
