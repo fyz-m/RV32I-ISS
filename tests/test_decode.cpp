e40d199a0a47fad045aa360d67bd237a3fd51d7b
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-05-03 20:30:49 +0400

    Rename test case structs to eliminate naming conflicts across test files

--- tests/test_decode.cpp
+++ tests/test_decode.cpp
@@ -4,18 +4,18 @@
 #include "../src/headers/decode.hpp"
 
 
-struct RtypeCase {
+struct Rtype_Decode_Case {
     uint32_t instruction;
     uint8_t expected_rd, expected_rs1, expected_rs2;
     OPERATION expected_op; 
     std::string test_name;
 };
 
-class RtypeTest : public ::testing::TestWithParam<RtypeCase> {};
+class RtypeTest : public ::testing::TestWithParam<Rtype_Decode_Case> {};
 
 TEST_P(RtypeTest, decode_R_type_instructions)
 {
-    RtypeCase tc = GetParam();
+    Rtype_Decode_Case tc = GetParam();
 
     DecodedInstruction fields;
     fields.raw_inst = tc.instruction;
@@ -28,20 +28,20 @@ TEST_P(RtypeTest, decode_R_type_instructions)
     EXPECT_EQ(fields.Operation, tc.expected_op);
 }
 
-INSTANTIATE_TEST_SUITE_P(R_TYPE_DECODE_TEST, RtypeTest,
+INSTANTIATE_TEST_SUITE_P(R_TYPE, RtypeTest,
 
     ::testing::Values(
-      RtypeCase {0x003100b3, 1, 2, 3, OPERATION::ADD, "add_basic"},  // add x1, x2, x3
-      RtypeCase {0x403100b3, 1, 2, 3 , OPERATION::SUB, "sub_basic"} // sub x1, x2, x3
+      Rtype_Decode_Case {0x003100b3, 1, 2, 3, OPERATION::ADD, "add_basic"},  // add x1, x2, x3
+      Rtype_Decode_Case {0x403100b3, 1, 2, 3 , OPERATION::SUB, "sub_basic"} // sub x1, x2, x3
     ),
 
-    [](const ::testing::TestParamInfo<RtypeCase>& info) {
+    [](const ::testing::TestParamInfo<Rtype_Decode_Case>& info) {
       return info.param.test_name;
       }
 );
 
 
-struct ItypeCase {
+struct Itype_Decode_Case {
     uint32_t instruction;
     uint8_t expected_rd, expected_rs1; 
     int32_t expected_imm;
@@ -49,11 +49,11 @@ struct ItypeCase {
     std::string test_name; 
 };
 
-class ItypeTest : public ::testing::TestWithParam<ItypeCase> {};
+class ItypeTest : public ::testing::TestWithParam<Itype_Decode_Case> {};
 
 TEST_P(ItypeTest, decode_I_type_instructions)
 {
-    ItypeCase tc = GetParam();
+    Itype_Decode_Case tc = GetParam();
 
     DecodedInstruction fields;
     fields.raw_inst = tc.instruction;
@@ -66,29 +66,29 @@ TEST_P(ItypeTest, decode_I_type_instructions)
     EXPECT_EQ(fields.Operation, tc.expected_op);
 }
 
-INSTANTIATE_TEST_SUITE_P(I_TYPE_DECODE_TEST, ItypeTest,
+INSTANTIATE_TEST_SUITE_P(I_TYPE, ItypeTest,
 
     ::testing::Values(
-      ItypeCase {0x02810093, 1, 2, 40, OPERATION::ADDI, "addi_basic"},  // addi x1, x2, 40
-      ItypeCase {0x00028503, 10, 5, 0 , OPERATION::LB, "lb_basic"}, // lb x10, 0(x5)
-      ItypeCase{0x001f1f93, 31, 30, 1, OPERATION::SLLI, "slli_basic"}, // slli x31, x30, 1
-      ItypeCase{0x01409093, 1, 1, 20, OPERATION::SLLI, "slli_basic_2"}, // slli x1, x1, 20
-      ItypeCase{0xff605003, 0, 0, -10, OPERATION::LHU, "lhu_basic"}, // lhu x0, -10(x0)
+      Itype_Decode_Case {0x02810093, 1, 2, 40, OPERATION::ADDI, "addi_basic"},  // addi x1, x2, 40
+      Itype_Decode_Case {0x00028503, 10, 5, 0 , OPERATION::LB, "lb_basic"}, // lb x10, 0(x5)
+      Itype_Decode_Case{0x001f1f93, 31, 30, 1, OPERATION::SLLI, "slli_basic"}, // slli x31, x30, 1
+      Itype_Decode_Case{0x01409093, 1, 1, 20, OPERATION::SLLI, "slli_basic_2"}, // slli x1, x1, 20
+      Itype_Decode_Case{0xff605003, 0, 0, -10, OPERATION::LHU, "lhu_basic"}, // lhu x0, -10(x0)
       // Corner case: both srli and srai have the same opcode and funct3
-      ItypeCase{0x00abdc93, 25, 23, 10, OPERATION::SRLI, "srli_basic"}, // srli x25, x23, 10  
-      ItypeCase{0x4081d613, 12, 3, 8, OPERATION::SRAI, "srai_basic"}, // srai x12, x3, 8 
-      ItypeCase{0x4020d093, 1, 1, 2, OPERATION::SRAI, "srai_basic_2"}, // srai x1, x1, 2  
+      Itype_Decode_Case{0x00abdc93, 25, 23, 10, OPERATION::SRLI, "srli_basic"}, // srli x25, x23, 10  
+      Itype_Decode_Case{0x4081d613, 12, 3, 8, OPERATION::SRAI, "srai_basic"}, // srai x12, x3, 8 
+      Itype_Decode_Case{0x4020d093, 1, 1, 2, OPERATION::SRAI, "srai_basic_2"}, // srai x1, x1, 2  
       // Test immediate
-      ItypeCase {0xfec10093, 1, 2, -20, OPERATION::ADDI, "sign_ext"},  // addi x1, x2, -20
-      ItypeCase {0xfff10093, 1, 2, -1, OPERATION::ADDI, "sign_ext_2"} // addi x1, x2, -1
+      Itype_Decode_Case {0xfec10093, 1, 2, -20, OPERATION::ADDI, "sign_ext"},  // addi x1, x2, -20
+      Itype_Decode_Case {0xfff10093, 1, 2, -1, OPERATION::ADDI, "sign_ext_2"} // addi x1, x2, -1
     ),
 
-    [](const ::testing::TestParamInfo<ItypeCase>& info) {
+    [](const ::testing::TestParamInfo<Itype_Decode_Case>& info) {
       return info.param.test_name;
       }
 );
 
-struct StypeCase {
+struct Stype_Decode_Case {
     uint32_t instruction;
     uint8_t expected_rs2; 
     int32_t expected_imm;
@@ -97,11 +97,11 @@ struct StypeCase {
     std::string test_name; 
 };
 
-class StypeTest : public ::testing::TestWithParam<StypeCase> {};
+class StypeTest : public ::testing::TestWithParam<Stype_Decode_Case> {};
 
 TEST_P(StypeTest, decode_S_type_instructions)
 {
-    StypeCase tc = GetParam();
+    Stype_Decode_Case tc = GetParam();
 
     DecodedInstruction fields;
     fields.raw_inst = tc.instruction;
@@ -114,24 +114,24 @@ TEST_P(StypeTest, decode_S_type_instructions)
     EXPECT_EQ(fields.Operation, tc.expected_op);
 }
 
-INSTANTIATE_TEST_SUITE_P(S_TYPE_DECODE_TEST, StypeTest,
+INSTANTIATE_TEST_SUITE_P(S_TYPE, StypeTest,
 
     ::testing::Values(
-      StypeCase {0x19488823, 20, 400, 17, OPERATION::SB, "sb_basic" }, // sb x20, 400(x17)
-      StypeCase {0x00a01023, 10, 0, 0, OPERATION::SH, "sh_basic" }, // sh x10, 0(x0)
-      StypeCase {0x7e09a623, 0, 2028, 19, OPERATION::SW, "sw_basic" }, // sw x0, 2028(x19)
+      Stype_Decode_Case {0x19488823, 20, 400, 17, OPERATION::SB, "sb_basic" }, // sb x20, 400(x17)
+      Stype_Decode_Case {0x00a01023, 10, 0, 0, OPERATION::SH, "sh_basic" }, // sh x10, 0(x0)
+      Stype_Decode_Case {0x7e09a623, 0, 2028, 19, OPERATION::SW, "sw_basic" }, // sw x0, 2028(x19)
 
-      StypeCase {0x80208023, 2, -2048, 1, OPERATION::SB, "sb_negative_imm" },  // sb x2, -2048(x1)
-      StypeCase {0xfdf09723, 31, -50, 1, OPERATION::SH, "sh_negative_imm" },  // sh x31, -50(x1)
-      StypeCase {0xfef6afa3, 15, -1, 13, OPERATION::SW, "sw_negative_imm" }   // sw x15, -1(x13)
+      Stype_Decode_Case {0x80208023, 2, -2048, 1, OPERATION::SB, "sb_negative_imm" },  // sb x2, -2048(x1)
+      Stype_Decode_Case {0xfdf09723, 31, -50, 1, OPERATION::SH, "sh_negative_imm" },  // sh x31, -50(x1)
+      Stype_Decode_Case {0xfef6afa3, 15, -1, 13, OPERATION::SW, "sw_negative_imm" }   // sw x15, -1(x13)
     ),
 
-    [](const ::testing::TestParamInfo<StypeCase>& info) {
+    [](const ::testing::TestParamInfo<Stype_Decode_Case>& info) {
       return info.param.test_name;
       }
 );
 
-struct BtypeCase {
+struct Btype_Decode_Case {
     uint32_t instruction;
     uint8_t expected_rs1; 
     uint8_t expected_rs2;
@@ -140,11 +140,11 @@ struct BtypeCase {
     std::string test_name; 
 };
 
-class BtypeTest : public ::testing::TestWithParam<BtypeCase> {};
+class BtypeTest : public ::testing::TestWithParam<Btype_Decode_Case> {};
 
 TEST_P(BtypeTest, decode_B_type_instructions)
 {
-    BtypeCase tc = GetParam();
+    Btype_Decode_Case tc = GetParam();
 
     DecodedInstruction fields;
     fields.raw_inst = tc.instruction;
@@ -157,31 +157,31 @@ TEST_P(BtypeTest, decode_B_type_instructions)
     EXPECT_EQ(fields.Operation, tc.expected_op);
 }
 
-INSTANTIATE_TEST_SUITE_P(B_TYPE_DECODE_TEST, BtypeTest,
+INSTANTIATE_TEST_SUITE_P(B_TYPE, BtypeTest,
 
     ::testing::Values(
-      BtypeCase{0x014000e3, 0, 20, 2048, OPERATION::BEQ, "beq_basic"}, // beq x0, x20, 2048
-      BtypeCase{0x00109663, 1,1, 12, OPERATION::BNE, "bne_basic"}, // bne x1, x1, 12
-      BtypeCase{0x0010c663, 1,1, 12, OPERATION::BLT, "blt_basic"}, // blt x1, x1, 12
-      BtypeCase{0x0010d663, 1,1, 12, OPERATION::BGE, "bge_basic"}, // bge x1, x1, 12
-      BtypeCase{0x0010e663, 1,1, 12, OPERATION::BLTU, "bltu_basic"}, // bltu x1, x1, 12
-      BtypeCase{0x0010f663, 1,1, 12, OPERATION::BGEU, "bgeu_basic"}, // bgeu x1, x1, 12
+      Btype_Decode_Case{0x014000e3, 0, 20, 2048, OPERATION::BEQ, "beq_basic"}, // beq x0, x20, 2048
+      Btype_Decode_Case{0x00109663, 1,1, 12, OPERATION::BNE, "bne_basic"}, // bne x1, x1, 12
+      Btype_Decode_Case{0x0010c663, 1,1, 12, OPERATION::BLT, "blt_basic"}, // blt x1, x1, 12
+      Btype_Decode_Case{0x0010d663, 1,1, 12, OPERATION::BGE, "bge_basic"}, // bge x1, x1, 12
+      Btype_Decode_Case{0x0010e663, 1,1, 12, OPERATION::BLTU, "bltu_basic"}, // bltu x1, x1, 12
+      Btype_Decode_Case{0x0010f663, 1,1, 12, OPERATION::BGEU, "bgeu_basic"}, // bgeu x1, x1, 12
       
       // Immediate tests 
-      BtypeCase{0xfe108ae3, 1,1, -12, OPERATION::BEQ, "imm_negative"}, // bge x1, x1, -12 
-      BtypeCase{0x00108263, 1, 1, 4, OPERATION::BEQ, "imm_min_positive"}, // beq x1, x1, 4
-      BtypeCase{0xfe108ee3, 1, 1, -4, OPERATION::BEQ, "imm_min_negative"}, // beq x1, x1, -4
-      BtypeCase{0x81400063, 0, 20, -4096, OPERATION::BEQ, "imm_min"}, // beq x0, x20, -4096
-      BtypeCase{0x7f400fe3, 0, 20, 4094, OPERATION::BEQ, "imm_max"} // beq x0, x20, 4094
+      Btype_Decode_Case{0xfe108ae3, 1,1, -12, OPERATION::BEQ, "imm_negative"}, // bge x1, x1, -12 
+      Btype_Decode_Case{0x00108263, 1, 1, 4, OPERATION::BEQ, "imm_min_positive"}, // beq x1, x1, 4
+      Btype_Decode_Case{0xfe108ee3, 1, 1, -4, OPERATION::BEQ, "imm_min_negative"}, // beq x1, x1, -4
+      Btype_Decode_Case{0x81400063, 0, 20, -4096, OPERATION::BEQ, "imm_min"}, // beq x0, x20, -4096
+      Btype_Decode_Case{0x7f400fe3, 0, 20, 4094, OPERATION::BEQ, "imm_max"} // beq x0, x20, 4094
       
     ),
 
-    [](const ::testing::TestParamInfo<BtypeCase>& info) {
+    [](const ::testing::TestParamInfo<Btype_Decode_Case>& info) {
       return info.param.test_name;
       }
 );
 
-struct UtypeCase {
+struct Utype_Decode_Case {
     uint32_t instruction;
     uint8_t expected_rd; 
     int32_t expected_imm;
@@ -189,11 +189,11 @@ struct UtypeCase {
     std::string test_name; 
 };
 
-class UtypeTest : public ::testing::TestWithParam<UtypeCase> {};
+class UtypeTest : public ::testing::TestWithParam<Utype_Decode_Case> {};
 
 TEST_P(UtypeTest, decode_U_type_instructions)
 {
-    UtypeCase tc = GetParam();
+    Utype_Decode_Case tc = GetParam();
 
     DecodedInstruction fields;
     fields.raw_inst = tc.instruction;
@@ -205,26 +205,26 @@ TEST_P(UtypeTest, decode_U_type_instructions)
     EXPECT_EQ(fields.Operation, tc.expected_op);
 }
 
-INSTANTIATE_TEST_SUITE_P(U_TYPE_DECODE_TEST, UtypeTest,
+INSTANTIATE_TEST_SUITE_P(U_TYPE, UtypeTest,
 
     ::testing::Values(
-      UtypeCase{0x000142b7, 5, 20, OPERATION::LUI, "lui_basic"}, // lui x5, 20
-      UtypeCase{0x00014297, 5, 20, OPERATION::AUIPC, "auipc_basic"}, // auipc x5, 20
+      Utype_Decode_Case{0x000142b7, 5, 20, OPERATION::LUI, "lui_basic"}, // lui x5, 20
+      Utype_Decode_Case{0x00014297, 5, 20, OPERATION::AUIPC, "auipc_basic"}, // auipc x5, 20
 
       // Immediate tests
-      UtypeCase{0xffe0c0b7, 1, -500,OPERATION::LUI, "u_type_imm_negative"}, // lui, x1, -500
-      UtypeCase{0x800000b7, 1, -524288,OPERATION::LUI, "u_type_imm_min"}, // lui, x1, -524288
-      UtypeCase{0x7ffff0b7, 1, 524287,OPERATION::LUI, "u_type_imm_max"} // lui, x1, 524287
+      Utype_Decode_Case{0xffe0c0b7, 1, -500,OPERATION::LUI, "u_type_imm_negative"}, // lui, x1, -500
+      Utype_Decode_Case{0x800000b7, 1, -524288,OPERATION::LUI, "u_type_imm_min"}, // lui, x1, -524288
+      Utype_Decode_Case{0x7ffff0b7, 1, 524287,OPERATION::LUI, "u_type_imm_max"} // lui, x1, 524287
 
     ),
 
-    [](const ::testing::TestParamInfo<UtypeCase>& info) {
+    [](const ::testing::TestParamInfo<Utype_Decode_Case>& info) {
       return info.param.test_name;
       }
 );
 
 
-struct JtypeCase {
+struct Jtype_Decode_Case {
     uint32_t instruction;
     uint8_t expected_rd; 
     int32_t expected_imm;
@@ -232,11 +232,11 @@ struct JtypeCase {
     std::string test_name; 
 };
 
-class JtypeTest : public ::testing::TestWithParam<JtypeCase> {};
+class JtypeTest : public ::testing::TestWithParam<Jtype_Decode_Case> {};
 
 TEST_P(JtypeTest, decode_J_type_instructions)
 {
-    JtypeCase tc = GetParam();
+    Jtype_Decode_Case tc = GetParam();
 
     DecodedInstruction fields;
     fields.raw_inst = tc.instruction;
@@ -248,20 +248,20 @@ TEST_P(JtypeTest, decode_J_type_instructions)
     EXPECT_EQ(fields.Operation, tc.expected_op);
 }
 
-INSTANTIATE_TEST_SUITE_P(J_TYPE_DECODE_TEST, JtypeTest,
+INSTANTIATE_TEST_SUITE_P(J_TYPE, JtypeTest,
 
     ::testing::Values(
-        JtypeCase{0x0c800a6f, 20, 200, OPERATION::JAL, "jal_basic"}, // jal x20, 200
+        Jtype_Decode_Case{0x0c800a6f, 20, 200, OPERATION::JAL, "jal_basic"}, // jal x20, 200
 
         // Immediate tests
-        JtypeCase{0xf39ffa6f, 20, -200, OPERATION::JAL, "jal_imm_negative"}, // jal x20, -200
-        JtypeCase{0x80000a6f, 20, -1048576, OPERATION::JAL, "jal_imm_negative_max"}, // jal x20, -1048576
-        JtypeCase{0xffdffa6f, 20, -4, OPERATION::JAL, "jal_imm_negative_min"}, // jal x20, -4
-        JtypeCase{0x00400a6f, 20, 4, OPERATION::JAL, "jal_imm_positive_min"}, // jal x20, 4
-        JtypeCase{0x7ffffa6f, 20, 1048574, OPERATION::JAL, "jal_imm_positive_max"} // jal x20, 1048574
+        Jtype_Decode_Case{0xf39ffa6f, 20, -200, OPERATION::JAL, "jal_imm_negative"}, // jal x20, -200
+        Jtype_Decode_Case{0x80000a6f, 20, -1048576, OPERATION::JAL, "jal_imm_negative_max"}, // jal x20, -1048576
+        Jtype_Decode_Case{0xffdffa6f, 20, -4, OPERATION::JAL, "jal_imm_negative_min"}, // jal x20, -4
+        Jtype_Decode_Case{0x00400a6f, 20, 4, OPERATION::JAL, "jal_imm_positive_min"}, // jal x20, 4
+        Jtype_Decode_Case{0x7ffffa6f, 20, 1048574, OPERATION::JAL, "jal_imm_positive_max"} // jal x20, 1048574
     ),
 
-    [](const ::testing::TestParamInfo<JtypeCase>& info) {
+    [](const ::testing::TestParamInfo<Jtype_Decode_Case>& info) {
       return info.param.test_name;
       }
 );
