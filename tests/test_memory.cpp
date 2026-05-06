dc9e6807694cf0b92fa0bd819f72cf30ed7c1769
fyz.m <237607300+fyz-m@users.noreply.github.com>
2026-05-06 09:42:56 +0400

    Add tests for Memory class

--- tests/test_memory.cpp
+++ tests/test_memory.cpp
@@ -1,4 +1,5 @@
 #include "gtest/gtest.h"
+#include <cstdint>
 #include "../src/headers/memory.hpp"
 
 class Registerfile_test : public ::testing::Test 
@@ -51,4 +52,65 @@ TEST_F(Registerfile_test, throws_exception_on_invalid_register_index)
 
   ASSERT_ANY_THROW(rf->Read(-1));
   ASSERT_ANY_THROW(rf->Read(32));
+}
+
+class MemoryTest : public ::testing::Test 
+{
+  protected:
+
+    void SetUp() override 
+    {
+     memory = new Memory(128);
+    }
+
+    void TearDown() override
+    {
+      delete memory;
+    }
+
+    Memory* memory;
+};
+
+TEST_F(MemoryTest, can_read_and_write_bytes)
+{
+  memory->Write(static_cast<uint8_t>(0xAB), 1);
+  ASSERT_EQ(memory->Read_Byte(1), 0xAB);
+
+  memory->Write(static_cast<uint8_t>(0xFFFFFFFF), 2);
+  ASSERT_EQ(memory->Read_Byte(2), 0xFF);
+}
+
+TEST_F(MemoryTest, can_read_and_write_words)
+{
+  memory->Write(static_cast<uint32_t>(0xDEADBEEF), 1);
+  ASSERT_EQ(memory->Read_Word(1), 0xDEADBEEF);
+  ASSERT_EQ(memory->Read_Byte(1), 0xEF);
+  ASSERT_EQ(memory->Read_halfWord(1), 0xBEEF);
+
+  memory->Write(static_cast<uint32_t>(0xFFFFFFFF), 4);
+  ASSERT_EQ(memory->Read_Word(4), 0xFFFFFFFF);
+}
+
+TEST_F(MemoryTest, can_read_and_write_halfword)
+{
+  memory->Write(static_cast<uint16_t>(0xAB), 1);
+  ASSERT_EQ(memory->Read_halfWord(1), 0x00AB);
+
+  memory->Write(static_cast<uint16_t>(0xFFFFFFFF), 1);
+  ASSERT_EQ(memory->Read_halfWord(1), 0xFFFF);
+}
+
+TEST_F(MemoryTest, resizes_on_addresses_greater_than_current_length)
+{
+  memory->Write(static_cast<uint32_t>(0xABCDEF00), 4000);
+  ASSERT_EQ(memory->Read_Word(4000), 0xABCDEF00);
+  ASSERT_EQ(memory->Read_halfWord(4000), 0xEF00);
+  ASSERT_EQ(memory->Read_Byte(4000), 0x00);
+}
+
+TEST_F(MemoryTest, max_resize)
+{
+  // Memory object will take up 4 GB of heap storage
+  memory->Write(static_cast<uint32_t>(0x11), 0xFFFFFFFF);
+  ASSERT_EQ(memory->Read_Byte(0xFFFFFFFF), 0x11);
 }
